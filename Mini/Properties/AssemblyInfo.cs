18a87822cb348c0e3313aaa328871c803de5f680
Sam Fredrickson <kinghajj@gmail.com>
2010-04-08 12:41:41 -0700

    Changed version to 0.2.0.0.

--- Mini/Properties/AssemblyInfo.cs
+++ Mini/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // If the build and revision are set to '*' they will be updated automatically.
 
-[assembly: AssemblyVersion("0.1.3.2")]
+[assembly: AssemblyVersion("0.2.0.0")]
 
 // The following attributes are used to specify the signing key for the
 // assembly, if desired. See the Mono documentation for more information about
