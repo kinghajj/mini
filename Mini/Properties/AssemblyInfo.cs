b494622b519aa5c8924dbeaf92529053cdf60204
Sam Fredrickson <kinghajj@gmail.com>
2013-07-03 23:53:44 -0700

    Version 0.2.1.0.

--- Mini/Properties/AssemblyInfo.cs
+++ Mini/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Mini")]
 [assembly: AssemblyDescription("A .NET library for reading and writing INI files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Samuel Fredrickson")]
 [assembly: AssemblyProduct("Mini")]
 [assembly: AssemblyCopyright("(C) 2013 Samuel Fredrickson")]
 [assembly: AssemblyTrademark("")]
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // If the build and revision are set to '*' they will be updated automatically.
 
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
 
 // The following attributes are used to specify the signing key for the
 // assembly, if desired. See the Mono documentation for more information about
