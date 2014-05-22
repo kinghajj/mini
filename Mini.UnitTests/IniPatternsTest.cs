43cba58b81f6190085b1811732881b680206e338
Sam Fredrickson <kinghajj@gmail.com>
2014-05-21 21:40:10 -0700

    Change IniSettingsKeyWhitespaceParseTest to use a file instead of a hardcoded string.

--- Mini.UnitTests/IniPatternsTest.cs
+++ Mini.UnitTests/IniPatternsTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Mini.UnitTests
 {
@@ -11,26 +8,14 @@ namespace Mini.UnitTests
         [TestMethod]
         public void IniSettingKeyWhitespaceParseTest()
         {
-            const string testDocument =
-                @"[Section]
-                FirstKey=FirstValue
-                SecondKey= SecondValue
-                ThirdKey =ThirdValue
-                FourthKey = FourthValue
-                 FifthKey=FifthValue
-                Sixth Key = Sixth Value";
-
-            using (var iniMemoryStream = new MemoryStream(Encoding.UTF8.GetBytes(testDocument)))
-            {
-                var document = new IniDocument(new StreamReader(iniMemoryStream));
-                var section = document["Section"];
-                Assert.AreEqual(section["FirstKey"].Value,  "FirstValue");
-                Assert.AreEqual(section["SecondKey"].Value, "SecondValue");
-                Assert.AreEqual(section["ThirdKey"].Value,  "ThirdValue");
-                Assert.AreEqual(section["FourthKey"].Value, "FourthValue");
-                Assert.AreEqual(section["FifthKey"].Value,  "FifthValue");
-                Assert.AreEqual(section["Sixth Key"].Value, "Sixth Value");
-            }
+            var document = new IniDocument("IniSettingKeyWhitespaceParseTestDocument.ini");
+            var section = document["Section"];
+            Assert.AreEqual(section["FirstKey"].Value,  "FirstValue");
+            Assert.AreEqual(section["SecondKey"].Value, "SecondValue");
+            Assert.AreEqual(section["ThirdKey"].Value,  "ThirdValue");
+            Assert.AreEqual(section["FourthKey"].Value, "FourthValue");
+            Assert.AreEqual(section["FifthKey"].Value,  "FifthValue");
+            Assert.AreEqual(section["Sixth Key"].Value, "Sixth Value");
         }
     }
 }
