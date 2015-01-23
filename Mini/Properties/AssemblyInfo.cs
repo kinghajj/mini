dca087f9ef52d7f890f5a6d8706cfae6568703f4
Sam Fredrickson <kinghajj@gmail.com>
2015-01-22 22:10:45 -0800

    0.4.1.0

--- Mini/Properties/AssemblyInfo.cs
+++ Mini/Properties/AssemblyInfo.cs
@@ -28,14 +28,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Samuel Fredrickson")]
 [assembly: AssemblyProduct("Mini")]
-[assembly: AssemblyCopyright("(C) 2014 Samuel Fredrickson")]
+[assembly: AssemblyCopyright("(C) 2015 Samuel Fredrickson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // If the build and revision are set to '*' they will be updated automatically.
 
-[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.4.1.0")]
 
 // The following attributes are used to specify the signing key for the
 // assembly, if desired. See the Mono documentation for more information about
