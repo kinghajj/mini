b5c31e34d31b595947153d2303b9455bd5dbe1ec
Sam Fredrickson <kinghajj@gmail.com>
2015-03-19 19:08:09 -0700

    Add simple tests for (de)serialization.

---
+++ Mini.UnitTests/IniSerializerTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mini.UnitTests
+{
+    [TestClass]
+    public class IniSerializerTest
+    {
+        [IniValueContainer]
+        private class Example
+        {
+            [IniValue("Woot", "Foo", "Bar")]
+            public string WootFoo { get; set; }
+        }
+
+        private readonly IniSerializer<Example> _exampleSerializer;
+
+        public IniSerializerTest()
+        {
+            _exampleSerializer = IniSerializer<Example>.New();
+        }
+
+        [TestMethod]
+        public void TestSerialization()
+        {
+            var example = new Example { WootFoo = "Baz" };
+            var serialized = new IniDocument();
+            _exampleSerializer.Serialize(example, serialized);
+            Assert.AreEqual(example.WootFoo, serialized["Woot"]["Foo"].Value);
+        }
+
+        [TestMethod]
+        public void TestDeserialization()
+        {
+            var ini = new IniDocument();
+            ini["Woot"]["Foo"].Value = "Hello";
+            var deserialized = _exampleSerializer.Deserialize(ini);
+            Assert.AreEqual(ini["Woot"]["Foo"].Value, deserialized.WootFoo);
+        }
+    }
+}
